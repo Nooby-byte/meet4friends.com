f3c62422d5356acb995f414f4f2424812d1b0090
Nooby-byte <77455451+Nooby-byte@users.noreply.github.com>
2021-03-03 16:34:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function checkPass(){
+    if(document.getElementById("pass").text = "gamer"){
+        document.getElementById("go").href = "meet.html"
+    }else{
+        document.getElementById("pass").value="kjnknsd"
+    }
+}
