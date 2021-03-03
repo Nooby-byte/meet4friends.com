e9352004dacdfe61bc1b0b650ae304b2419febd4
Nooby-byte <77455451+Nooby-byte@users.noreply.github.com>
2021-03-03 16:52:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function checkPass(){
     if(document.getElementById("pass").text = "gamer"){
-        document.getElementById("go").href = "meet.html"
+        document.getElementById("go").href = "https://meet.google.com/eug-swkt-wjr"
     }else{
         document.getElementById("pass").value="kjnknsd"
     }
